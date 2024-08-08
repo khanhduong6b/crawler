615a029ee653fd35da42f99745bb42985fcdfee3
Duong Ho <khanhduong6b@gmail.com>
2024-08-08 22:28:52 +0700

    fix: get all data

--- routes/stock/stockController.js
+++ routes/stock/stockController.js
@@ -35,7 +35,7 @@ function StockController() {
         },
         getAllData: async (req,res) => {
             const symbol = req.query.symbol
-            const data = await StockTransaction.find({symbol: symbol}, {_id: 0, symbol: 1, tradingDate: 1, time: 1, open: 1, high: 1, low: 1, close: 1}).lean()
+            const data = await StockTransaction.find({symbol: symbol}, {_id: 0, symbol: 1, tradingDate: 1, time: 1, open: 1, high: 1, low: 1, close: 1, volume: 1}).lean()
             // convert ele
             return res.status(200).json({ data })
         },
