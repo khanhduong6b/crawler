17994af5499ebb5d1c8b9b1dfb924bea108ab916
Duong Ho <khanhduong6b@gmail.com>
2024-08-08 22:47:58 +0700

    fix : crawl test

--- tests/crawl.js
+++ tests/crawl.js
@@ -22,25 +22,25 @@ mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB).then(async () => {
    // //await StockController.storeStock();
 
-   // let fdate = '01/01/2024'
-   // //const data = await Crawler.getIntradayData('TCI', '01/02/2024', '29/02/2024')
-   // const listStock = await Stock.find({symbol: "TCI"}).lean()
-   // // //console.log(listStock)
+   let fdate = '01/01/2024'
+   //const data = await Crawler.getIntradayData('TCI', '01/02/2024', '29/02/2024')
+   const listStock = await Stock.find({symbol: "TCI"}).lean()
+   // //console.log(listStock)
    
-   // while (TimeUtil.compareDates(TimeUtil.getStrDate('DD/MM/YYYY', new Date()), fdate) == 1) {
-   //    if (TimeUtil.getDayOfWeek(fdate) != 'CHU NHAT' && TimeUtil.getDayOfWeek(fdate) != 'THU BAY') 
-   //    for (let i = 0; i < listStock.length; i++) {
-   //       const symbol = listStock[i].symbol
-   //       const data = await Crawler.getIntradayData(symbol, fdate, fdate)
-   //       if (data.length > 0)
-   //       await StockTransaction.insertMany(data)
-   //       await delay(1000)
-   //    } 
-   //    Logger.info('done crawl ' + fdate)
-   //    fdate = TimeUtil.getNextDate(fdate)
-   // }
+   while (TimeUtil.compareDates(TimeUtil.getStrDate('DD/MM/YYYY', new Date()), fdate) == 1) {
+      if (TimeUtil.getDayOfWeek(fdate) != 'CHU NHAT' && TimeUtil.getDayOfWeek(fdate) != 'THU BAY') 
+      for (let i = 0; i < listStock.length; i++) {
+         const symbol = listStock[i].symbol
+         const data = await Crawler.getIntradayData(symbol, fdate, fdate)
+         if (data.length > 0)
+         await StockTransaction.insertMany(data)
+         await delay(1000)
+      } 
+      Logger.info('done crawl ' + fdate)
+      fdate = TimeUtil.getNextDate(fdate)
+   }
 
-   // Logger.info('done crawl ' + fdate)
+   Logger.info('done crawl ' + fdate)
 
-   await StockController.storeNewData('TCI')
+   //await StockController.storeNewData('TCI')
 })
