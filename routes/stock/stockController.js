d56d3f1f94d1f2364b9acb577bb6bcf034f71f44
Duong Ho <khanhduong6b@gmail.com>
2024-08-18 21:18:16 +0700

    fix: getNewData

--- routes/stock/stockController.js
+++ routes/stock/stockController.js
@@ -63,7 +63,7 @@ function StockController() {
                             data: dataRes.sort((a, b) => a.time.localeCompare(b.time))
                         })
                     }
-                    return res.status(200).json([])
+                    return res.status(200).json({ data: []})
                 })
             } catch (error) {
                 return res.status(500).json({ error })
