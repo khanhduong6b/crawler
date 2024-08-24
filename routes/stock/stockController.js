48137f4174b8ae2d0edf9eade0b89e47573dc97a
Duong Ho <khanhduong6b@gmail.com>
2024-08-24 22:30:27 +0700

    fix: stockController

--- routes/stock/stockController.js
+++ routes/stock/stockController.js
@@ -55,17 +55,15 @@ function StockController() {
             try {
                 const symbol = req.query.symbol
                 if (!symbol) return res.status(200).json({ data: [] })
-                return RedisService.receiveTokenInRedis(symbol).then(async data => {
-                    if (data) {
-                        await RedisService.clearDataByKey(symbol);
-                        const dataRes = JSON.parse(data)
-                        return res.status(200).json({
-                            data: dataRes.sort((a, b) => a.time.localeCompare(b.time))
-                        })
-                    }
-                    return res.status(200).json({ data: [] })
-                })
+                
+                const data = await StockTransaction.find(
+                    { symbol: symbol, $or: [{ tradingDate: TimeUtil.getStrDate('YYYY-MM-DD', new Date())}, { tradingDate: TimeUtil.getStrDate('YYYY-MM-DD', new Date(new Date() - 86400))}] },
+                    { _id: 0, symbol: 1, tradingDate: 1, time: 1, open: 1, high: 1, low: 1, close: 1, volume: 1 }
+                ).sort({ time: 1 }).lean();
+
+                return res.status(200).json({ data });
             } catch (error) {
+                console.log(error)
                 return res.status(500).json({ error })
             }
         },
@@ -101,7 +99,7 @@ function StockController() {
                 if (dataOld.length != dataNew.length) {
                     await StockTransaction.deleteMany({ symbol: symbol, tradingDate: TimeUtil.getStrDate('YYYY-MM-DD', currentDate) })
                     await StockTransaction.insertMany(dataNew)
-                    await RedisService.storeTokenInRedis(symbol, JSON.stringify(dataNew))
+                    //await RedisService.storeTokenInRedis(symbol, JSON.stringify(dataNew))
                 }
                 Logger.info('storeNewData - success')
             } catch (error) {
